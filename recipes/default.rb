fb82813640f3f300c4b4530df935ea670a7708d4
Cookstyle <cookbooks@chef.io>
2021-06-21 13:49:30 -0700

    Cookstyle Bot Auto Corrections with Cookstyle 7.14.0 This change is automatically generated by the Cookstyle Bot using the latest version of Cookstyle (7.14.0). Adopting changes suggested by Cookstyle improves cookbook readability, avoids common coding mistakes, and eases upgrades to newer versions of the Chef Infra Client.
    Signed-off-by: Cookstyle <cookbooks@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: site_config-1
+# Cookbook:: site_config-1
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright:: (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'base_web'
 
